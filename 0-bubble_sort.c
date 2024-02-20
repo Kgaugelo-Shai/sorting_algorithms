0499fc96525df54e940dfe13497aeba897e0f5d1
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-02-20 09:20:03 +0200

    betty changes task 0 and 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,28 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - sorts a an array in ascending order
+ * using the bubble sort method
+ * @array: array to sort
+ * @size: the size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-  int tmp;
-  size_t i, j;
+	int tmp;
+	size_t i, j;
 
-  if (array == NULL || size < 2)
-    return;
-  for (i = 0; i < size; i++)
-    {
-      for (j = 0; j < size - 1; j++)
-        {
-           if (array[j] > array[j + 1])
-            {
-              tmp = array[j];
-              array[j] = array[j + 1];
-              array[j + 1] = tmp;
-              print_array(array, size);
-            }
-          }
-      }
-  }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,23 +1,30 @@
 #include "sort.h"
+/**
+ * getSize - returns the size of the list
+ * @list: list to analyze
+ * Return: size of list as an integer
+ */
+int getSize(listint_t **list)
+{
+	int count = 0;
+	listint_t *temp = *list;
 
-int getSize(listint_t **list) {
-  int count = 0;
-  listint_t *temp = *list;
-
-  while (temp != NULL) {
-    count++;
-    temp = temp->next;
-  }
-  return count;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
 }
 
 /**
- * insertion_sort_list - function to sort a doubly linked list of integers using
+ * insertion_sort_list - function to sort a doubly linked list of integers
+ * using
  * the sort insertion algotithm
  * @list: pointer to the head of the list
  */
 
-/**
+/*
  * get length of list and check if list is empty and check length of list
  * if list is not empty, traverse the list
  * if list is empty, return
@@ -30,58 +37,43 @@ int getSize(listint_t **list) {
  * current node and increase the count
  */
 
-void insertion_sort_list(listint_t **list) {
-  int length = getSize(list);
-  listint_t *outter;
-
-  if (list == NULL || length < 2) {
-    return;
-  }
-  outter = (*list)->next;
-
-  while (outter != NULL)
-    {
-      listint_t *temp_key = outter;
-      listint_t *inner = outter->prev;
-
-      while (inner != NULL && inner->n > temp_key->n)
-        {
-          
-          if (inner->prev != NULL) {
-              inner->prev->next = temp_key;
-          } else {
-              *list = temp_key;
-          }
-
-          temp_key->prev = inner->prev;
-          inner->prev = temp_key;
-          inner->next = temp_key->next;
-
-          if (temp_key->next != NULL) {
-              temp_key->next->prev = inner;
-          }
-
-          temp_key->next = inner;
-
-          print_list(*list);
-          inner = temp_key->prev;
-        }
-       outter = outter->next;
-    }
-}
-
-/*void insertionSort(int arr[], int n)
+void insertion_sort_list(listint_t **list)
 {
-  int i, key, j;
+	int length = getSize(list);
+	listint_t *outter;
 
-  for (i = 1; i < n; i++) {
-    key = arr[i];
-    j = i - 1;
+	if (list == NULL || length < 2)
+		return;
 
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-}*/
+	outter = (*list)->next;
+
+	while (outter != NULL)
+	{
+		listint_t *temp_key = outter;
+		listint_t *inner = outter->prev;
+
+		while (inner != NULL && inner->n > temp_key->n)
+		{
+			if (inner->prev != NULL)
+			{
+				inner->prev->next = temp_key;
+			}
+			else
+			{
+				*list = temp_key;
+			}
+			temp_key->prev = inner->prev;
+			inner->prev = temp_key;
+			inner->next = temp_key->next;
+
+			if (temp_key->next != NULL)
+			{
+				temp_key->next->prev = inner;
+			}
+			temp_key->next = inner;
+			print_list(*list);
+			inner = temp_key->prev;
+		}
+		outter = outter->next;
+	}
+}
